308a24a0d823ab9b032559046e8c61e848372839
Renzo Damian <renzo.damian.go@gmail.com>
2022-09-13 01:55:41 -0500

    edit delay and addd hello background

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
 		"./src/components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				wiggle: 'wiggle .4s 1.9s 2 ease-in-out both ',
+				appear: 'appear 1.2s .7s ease-in both '
+			},
+			keyframes: {
+				wiggle: {
+					'0%, 100%': { transform: 'rotate(-3deg)' },
+					'50%': { transform: 'rotate(3deg)' },
+				},
+				appear: {
+					'0%': { opacity: '0%' },
+					'100%': { opacity: '100%' },
+				}
+			}
+		},
 	},
 	plugins: [],
 };
