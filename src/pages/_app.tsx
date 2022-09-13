57a11817ec2aa6e0f7471d32f300c154e2edd841
Renzo Damian <renzo.damian.go@gmail.com>
2022-09-13 00:32:45 -0500

    Add Scss and absolute path imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import 'styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
