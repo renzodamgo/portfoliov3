8e4f2b5c86f0b99456790662887106a6530ec480
Renzo Damian <renzo.damian.go@gmail.com>
2022-09-13 03:29:23 -0500

    show test blog titles

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,38 @@
+type postType = {
+	name: string;
+	title: string;
+};
+
+const Blog = ({ allPosts }: { allPosts: postType[] }) => {
+	return (
+		<div>
+			{allPosts.map((post) => (
+				<h1 key={post.name}>
+					{post.name} - {post.title}
+				</h1>
+			))}
+		</div>
+	);
+};
+
+export default Blog;
+
+export const getStaticProps = async () => {
+	const allPosts = [
+		{
+			name: 'Renzo',
+			title: 'Make of del protfolio',
+		},
+		{
+			name: 'Renzo',
+			title: 'Make of del protfolio2',
+		},
+		{
+			name: 'Renzo',
+			title: 'Make of del protfolio3',
+		},
+	];
+	return {
+		props: { allPosts },
+	};
+};
