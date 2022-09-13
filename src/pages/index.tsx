02b1efaf84b5434018b4a3eda5d39e67d423d8f8
Renzo Damian <renzo.damian.go@gmail.com>
2022-09-13 02:00:59 -0500

    add cursor pointer and none user selct to background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ const Home: NextPage = () => {
 			</Head>
 			<div className="main ">
 				<div className="container">
-					<div className="flex justify-center items-center h-screen w-screen flex-col gap-4 px-6 overflow-hidden">
-						<h1 className=" text-4xl md:text-6xl md:w-[600px] text-center  animate-appear">
+					<div className="flex justify-center items-center h-screen w-screen flex-col gap-4 px-6 overflow-hidden relative">
+						<h1 className=" text-4xl md:text-6xl md:w-[600px] text-center  animate-appear z-10">
 							<span className="flex justify-center items-center gap-2 md:gap-3">
 								Hola{' '}
 								<img
@@ -26,8 +26,14 @@ const Home: NextPage = () => {
 							Estoy creando un portfolio{' '}
 							<span className="font-semibold">Next Gen</span>.
 						</h1>
-						<p className=" animate-appear">Vuelva más tarde :)</p>
-						<div className="absolute top-0 left-0 text-4xl md:text-7xl font-bold opacity-10 text-justify h-screen w-screen overflow-hidden">
+						<p className=" animate-appear z-10">
+							Puede visitar la{' '}
+							<a className=" underline" href="#">
+								<span className=" font-semibold">v1</span> aquí
+							</a>
+							.
+						</p>
+						<div className="absolute top-0 left-0 text-4xl md:text-7xl font-bold opacity-10 text-justify h-screen w-screen overflow-hidden z-1 select-none">
 							Hola Hello 你好 Salut Привет Halo こんにちは Ciao Hola Hello 你好
 							Salut Привет Halo こんにちは Ciao Olá Hola Hello 你好 Salut Привет
 							Halo こんにちは Ciao Hola Hello 你好 Salut Привет Halo こんにちは
