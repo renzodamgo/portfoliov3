0616b8b4524651efe3f1cb34491c05a8786c2e71
Renzo Damian <rdamian@bevertec.com>
2023-01-16 04:14:52 -0500

    Add tailwind children variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
+
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 module.exports = {
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx}",
@@ -22,5 +24,9 @@ module.exports = {
 			}
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addVariant }) {
+			addVariant('ch', '&>*');
+		})
+	],
 };
