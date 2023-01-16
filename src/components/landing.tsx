c45b0cdd007cdca661ec903cc4eba35295aaa9f7
Renzo Damian <rdamian@bevertec.com>
2023-01-16 04:15:08 -0500

    add landing fake cards

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,7 +1,37 @@
 export const Landing = () => {
 	return (
 		<div>
-			<div className="container mx-auto"> Hello</div>
+			<div className="container px-6 md:mx-auto">
+				<div className="grid gap-4">
+					<h1 className="mt-10 text-4xl">Hola, soy Renzo Damian</h1>
+					<p>
+						Me gusta crear, investigar y romper cosas. Este es mi blog donde
+						comparto todo lo que he aprendo para recordarlo cuando me lo olvide
+						mañana.
+					</p>
+				</div>
+			</div>
+			<section>
+				<div className="container mt-10 px-6 md:mx-auto">
+					<div className="grid h-[70vh] gap-8 md:grid-cols-3 md:grid-rows-3 ch:bg-green-900">
+						<div className="flex  md:col-span-2 md:row-span-2 md:h-[100%] md:w-full">
+							ds
+						</div>
+						<div className="flex  md:col-span-1 md:row-span-1 md:h-[100%] md:w-full">
+							ds
+						</div>
+						<div className="flex  md:col-span-1 md:row-span-1 md:h-[100%] md:w-full">
+							ds
+						</div>
+						<div className="flex  md:col-span-2 md:row-span-1 md:h-[100%] md:w-full">
+							ds
+						</div>
+						<div className="flex  md:col-span-1 md:row-span-1 md:h-[100%] md:w-full">
+							ds
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 };
