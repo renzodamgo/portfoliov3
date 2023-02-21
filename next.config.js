43da49dd5b3589f3ff7018552ba963dc0aad00c9
Renzo Damian <rdamian@bevertec.com>
2023-02-20 23:30:03 -0500

    show only english translations

--- next.config.js
+++ next.config.js
@@ -3,14 +3,15 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	i18n: {
-		locales: ['es-ES', 'en-US'],
+		// locales: ['es-ES', 'en-US'],
+		locales: ['en-US'],
 		defaultLocale: 'en-US',
 	},
 	async redirects() {
 		return [
 			{
 				source: '/',
-				destination: '/about',
+				destination: '/projects',
 				permanent: true,
 			},
 		];
